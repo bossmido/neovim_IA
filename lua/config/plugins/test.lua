420dcedafeca676f89b294b3bf190a36f69780d5
kotikkir9 <kirillzhukau@outlook.com>
2025-02-07 12:50:25 +0100

    - Added nvim-lint.

--- lua/config/plugins/test.lua
+++ lua/config/plugins/test.lua
@@ -3,6 +3,22 @@ return {
         -- dir = "~/plugins/present.nvim",
     },
     {
+        'mfussenegger/nvim-lint',
+        config = function()
+            require('lint').linters_by_ft = {
+                python = { 'mypy' },
+            }
+
+            vim.api.nvim_create_user_command("Lint", function()
+                require("lint").try_lint()
+            end, {})
+
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function() require("lint").try_lint() end,
+            })
+        end
+    },
+    {
         'rmagatti/auto-session',
         lazy = false,
         keys = {

fa08828c265b77f9128c36e3b387ba7d8ada0d96
kotikkir9 <kirillzhukau@outlook.com>
2025-03-26 03:18:41 +0100

    Updated plugins.

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ if vim.fn.has("win32") == 1 then
     vim.opt.shellslash = true
 end
 
--- vim.diagnostic.config({ virtual_lines=true })
+vim.diagnostic.config({ virtual_text = true })
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"

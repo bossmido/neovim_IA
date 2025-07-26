b67bc50365c54442ad3bae9cc5798082c674cf66
bossmido <hemopho@gmail.com>
2025-07-26 16:29:56 +0200

    je pense qu'on peut se calmer (2)

---
+++ lua/config/plugins/nvim_tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*", -- optional: lock to latest stable release
+  lazy = false,  -- or `event = "VeryLazy"` to defer load
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- optional, for file icons
+  },
+  config = function()
+    require("nvim-tree").setup({
+      sort_by = "case_sensitive",
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    })
+  end,
+}

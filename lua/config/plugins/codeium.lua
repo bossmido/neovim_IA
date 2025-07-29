d523698727221c0bbaeee901fd6a620cafb79f16
bossmido <hemopho@gmail.com>
2025-07-29 12:26:03 +0200

    rajout de quelque oublies

---
+++ lua/config/plugins/codeium.lua
@@ -0,0 +1,13 @@
+return {
+  
+    "jcdickinson/codeium.nvim",
+    config = function()
+      require("codeium").setup({})
+      
+      -- Optional: Map keys to accept Codeium suggestions
+      vim.api.nvim_set_keymap("i", "<C-l>", 'v:lua.codeium.accept()', { expr = true, silent = true })
+      vim.api.nvim_set_keymap("i", "<C-]>", 'v:lua.codeium.dismiss()', { expr = true, silent = true })
+      vim.api.nvim_set_keymap("i", "<C-;>", 'v:lua.codeium.complete()', { expr = true, silent = true })
+    end,
+    event = "VeryLazy",
+}

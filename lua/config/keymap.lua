beac63c29684a9ad91b7c371eefa9928b1a27920
bossmido <hemopho@gmail.com>
2025-07-29 12:40:06 +0200

    neo tree s'ouvre normallement'

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -75,8 +75,8 @@ end, { desc = 'Toggle diagnostic virtual lines' })
 vim.keymap.set("n", "<leader>n", function() buffer_command("bnext") end)
 vim.keymap.set("n", "<leader>p", function() buffer_command("bprev") end)
 
-vim.keymap.set("n", "<C-n>", function() buffer_command("bnext") end)
-vim.keymap.set("n", "<C-p>", function() buffer_command("bprev") end)
+--vim.keymap.set("n", "<C-n>", function() buffer_command("bnext") end)
+--vim.keymap.set("n", "<C-p>", function() buffer_command("bprev") end)
 
 -- vim.keymap.set("n", "<leader>dd", function()
 vim.keymap.set("n", "<leader>bd", function()

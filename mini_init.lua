cdced54763edf50835ea89755ae564b33a39746d
bossmido <hemopho@gmail.com>
2025-07-30 14:39:09 +0200

    en fait si ca marche en projet 2

---
+++ mini_init.lua
@@ -0,0 +1,10 @@
+vim.g.lazyvim_blink_main = true
+require("config.lazy")
+require("config.keymap")
+require("config.autocmd")
+require("config.filetype")
+--ME MIENS
+require("config.cpp")
+
+
+require('lspconfig').rust_analyzer.setup{}

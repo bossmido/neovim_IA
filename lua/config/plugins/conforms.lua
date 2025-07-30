12073fd0e32dcf563a125a99fd3011cd39dc7651
bossmido <hemopho@gmail.com>
2025-07-30 10:19:14 +0200

    tunage de taille de notify

--- lua/config/plugins/conforms.lua
+++ lua/config/plugins/conforms.lua
@@ -4,5 +4,23 @@ return {
     formatters_by_ft = {
       cpp = { "clang_format" },
       c = { "clang_format" },
-  },
+  },    format_on_save = {
+      timeout_ms = 500,
+
+      lsp_fallback = true,
+    },   format = function(bufnr)
+      require("conform").format({
+        bufnr = bufnr,
+        async = false,
+        lsp_fallback = {
+          timeout_ms = 500,
+          --- 👇 explicitly pass utf-8
+          filter = function(client)
+            client.offset_encoding = "utf-8"
+            return true
+          end,
+
+        },
+      })
+    end,
 }}
--- lua/config/post_lazy.lua
+++ lua/config/post_lazy.lua
@@ -2,18 +2,25 @@
 local luasnip = require("luasnip")
 
 luasnip.config.set_config({
-  -- Use bordered windows with rounded corners
-  ext_opts = {
-    [require("luasnip.util.types").choiceNode] = {
-      active = {
-        virt_text = { { "●", "GruvboxGreen" } }, -- or other marker
-        hl_group = "Visual",
-        -- set window options for floating windows:
+    -- Use bordered windows with rounded corners
+    ext_opts = {
+        [require("luasnip.util.types").choiceNode] = {
+            active = {
+                virt_text = { { "●", "GruvboxGreen" } }, -- or other marker
+                hl_group = "Visual",
+                -- set window options for floating windows:
 
-        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
-        border = "rounded",
-      },
+                winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+                border = "rounded",
+            },
+        },
     },
-  },
 })
 -------------------------------------------------------------------------
+require("notify").setup({
+    -- Make sure stages support multiline
+    stages = "fade_in_slide_out",
+    -- or try "slide" or "fade"
+    max_width = 110,    -- adjust width if needed
+    max_height = 50000, -- adjust height if needed
+})

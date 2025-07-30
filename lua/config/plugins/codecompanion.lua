fe1381d6f76819e3bc252a17e5a6b2dc67994452
bossmido <hemopho@gmail.com>
2025-07-30 16:38:26 +0200

    ajouter un gestionnaire de paquet

---
+++ lua/config/plugins/codecompanion.lua
@@ -0,0 +1,27 @@
+return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("codecompanion").setup({
+            adapters = {
+                deepseek = function()
+                    return require("codecompanion.adapters").extend("deepseek", {
+                        env = {
+                            api_key = vim.fn.getenv("API_KEY_DEEPSEEK"),
+                        },
+                    })
+                end,
+            },
+
+            strategies = {
+
+                chat = { adapter = "deepseek", },
+                inline = { adapter = "deepseek" },
+                agent = { adapter = "deepseek" },
+            },
+        })
+    end
+}

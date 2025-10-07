00c104343d3473f0e8a3388553d9fd12e05ebecb
bossmido <hemopho@gmail.com>
2025-10-07 13:31:20 +0000

    semble correct

--- lua/plugins/possession.lua
+++ lua/plugins/possession.lua
@@ -19,9 +19,9 @@ return
                 tmp = true,
             },
             commands = {
-                save = "SessionSave",
-                load = "SessionLoad",
-                delete = "SessionDelete",
+                save = "PossessionSave",
+                load = "PossessionLoad",
+                delete = "PossessionDelete",
             },
         })
 
